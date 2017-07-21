9337d21966f818c09bcf4b161d3aa4dfa5b40dfe
Gracegrx <grace_grx@outlook.com>
2017-07-21 14:10:40 -0400

    first commit

---
+++ Intro of JavaScript/alert.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+var input1 = document.getElementById("input");
+//var v = input1.value;
+var btn = document.getElementById("btn");
+btn.onclick = alt;
+function alt() {
+    var v = input1.value;
+    alert(v);
+}
